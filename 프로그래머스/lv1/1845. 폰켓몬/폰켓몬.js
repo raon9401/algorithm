df766cd08cc3306f70647a3b152c74700516223b
raon9401 <raon9401@naver.com>
2022-08-10 21:44:01 +0900

    [level 1] Title: 폰켓몬, Time: 1.13 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,22 @@
+// nums -> 배열
+// 출력 : 입력받은 배열에서 고를수 있는 종류
+// 폰켓몬은 nums.length/2 만큼 고를 수 있다.
+
+function solution(nums) {
+    var answer = 0;
+    const obj = {};
+    let kinds = 0;
+    let canChoice = nums.length/2;
+    
+    nums.forEach(el => {obj[el] = true});
+    
+    kinds = Object.keys(obj).length;
+    if(canChoice >= kinds){
+        answer = kinds;
+    }else{
+        answer = canChoice;
+    }
+    
+    
+    return answer;
+}
