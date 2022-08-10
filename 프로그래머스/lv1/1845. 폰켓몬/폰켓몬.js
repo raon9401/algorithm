edfe8c6fdee2a69f181e021f1a2b91c974cb9c50
raon9401 <raon9401@naver.com>
2022-08-11 01:54:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -3,8 +3,15 @@
 // 폰켓몬은 nums.length/2 만큼 고를 수 있다.
 
 function solution(nums) {
-  const max = nums.length / 2;
-  const arr = [...new Set(nums)];
-
-  return arr.length > max ? max : arr.length
+    var answer = 0;
+    const obj = {};
+    let kinds = 0;
+    let canChoice = nums.length/2;
+    
+    nums.forEach(el => {obj[el] = true});
+    kinds = Object.keys(obj).length;
+    if(canChoice >= kinds) answer = kinds;
+    else answer = canChoice;
+    
+    return answer;
 }
