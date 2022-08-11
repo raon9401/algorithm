52edbe344a53fe6e34997cef4e35214881646950
raon9401 <raon9401@naver.com>
2022-08-11 20:36:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.98 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,28 @@
+// 완주하지 못한 단 한명의 이름을 출력
+// participant : 참가자
+// completion : 완주자
+
+function solution(participant, completion) {
+  var answer = '';
+  const obj = {};
+  // 완주한 사람들을 특정 값을 가진 객체로 변환.
+  for(let i of participant){
+      if(obj[i] === undefined){
+          obj[i] = 1;
+      } else {
+          obj[i] = obj[i]+1;
+      }
+  }
+  // 객체에서 완주자들은 0으로 바꿈
+  for(let i of completion){
+      if(obj[i] !== 0){
+          obj[i] = obj[i]-1
+      }
+  }
+  for(let property in obj){
+    if(obj[property] === 1){
+      answer = property;
+    }
+  }
+  return answer;
+}
