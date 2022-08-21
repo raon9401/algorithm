68b464b3f0f33716820777c43232bec33315aea3
raon9401 <raon9401@naver.com>
2022-08-22 02:39:17 +0900

    [level 2] Title: 올바른 괄호, Time: 6.75 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,16 +1,15 @@
 function solution(s) {
   let cnt = 0;
-
+  if(s[0] === ")" || s[s.length-1] === "("){
+    return false;
+  }
+  
   for (let i of s) {
-    if (i === "(") cnt++;
-    else if (i === ")") cnt--;
-    if (cnt < 0) {
-      return false;
-    }
+    if(i === "(") cnt++;
+    else if(i === ")") cnt--;
+      
+    if(cnt < 0) return false;
   }
-
-  if (cnt === 0) {
-    return true;
-  }
-  return false;
+  return cnt === 0
+ 
 }
