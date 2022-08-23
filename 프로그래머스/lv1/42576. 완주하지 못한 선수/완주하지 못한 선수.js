60e610142266126f55552cb6ff7636141156310d
raon9401 <raon9401@naver.com>
2022-08-23 19:21:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.06 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,18 @@
-// 완주하지 못한 단 한명의 이름을 출력
-// participant : 참가자
-// completion : 완주자
-
 function solution(participant, completion) {
   const obj = {};
-  // 완주한 사람들을 특정 값을 가진 객체로 변환.
-  for(let i of participant){
-      if(obj[i] === undefined){
-          obj[i] = 1;
-      } else {
-          obj[i]++;
-      }
+  for (let el of participant) {
+    // 객체에 키 : 벨류 할당
+    obj[el] = 0;
   }
-  // 객체에서 완주자들은 0으로 바꿈
-  for(let i of completion){
-      if(obj[i] !== 0){
-          obj[i]--;
-      }
+  for (let el of participant) {
+    // 참가한 사람 카운트
+    obj[el]++;
   }
-  for(let property in obj){
-    if(obj[property] === 1){
-      return property;
-    }
+  for (let el of completion) {
+    // 객체에서 완주한 사람 제외
+    obj[el]--;
+  }
+  for (let el of participant) {
+    if (obj[el] === 1) return el;
   }
 }
