f26d75b257f65e6b501da42e948fcda7423f85cc
raon9401 <raon9401@naver.com>
2022-09-05 17:24:44 +0900

    [level 2] Title: 땅따먹기, Time: 50.32 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,12 +1,8 @@
 function solution(land) {
   for (let i = 1; i < land.length; i++) {
     for (let j = 0; j < 4; j++) {
-      land[i][j] += Math.max(
-        ...land[i - 1].slice(0, j),
-        ...land[i - 1].slice(j + 1)
-      );
+      land[i][j] += Math.max(...land[i - 1].filter((el, index) => index !== j));
     }
   }
-    
   return Math.max(...land[land.length - 1]);
 }
