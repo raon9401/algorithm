63610eb1539900a4d645f7e6f094f7ad5c59ec95
raon9401 <raon9401@naver.com>
2022-09-06 00:44:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,19 +1,19 @@
 function solution(id_list, report, k) {
   var answer = {};
   let userMap = {};
-  report = [...new Set(report)]; // 한명이 다른 한명을 한번만 신고 가능.
-  for (let i = 0; i < id_list.length; i++) {
-    userMap[id_list[i]] = [];
-    answer[id_list[i]] = 0;
+  report = [...new Set(report)]; // 한명이 다른 한명을 한번만 신고 가능. 중복 제거
+  for (let i of id_list) {
+    userMap[i] = [];
+    answer[i] = 0;
   }
   for (let i = 0; i < report.length; i++) {
     report[i] = report[i].split(" ");
     userMap[report[i][1]].push(report[i][0]);
   }
-  for (let i = 0; i < id_list.length; i++) {
-    if (userMap[id_list[i]].length >= k) {
-      for (let j = 0; j < userMap[id_list[i]].length; j++) {
-        answer[userMap[id_list[i]][j]]++;
+  for (let i of id_list) {
+    if (userMap[i].length >= k) {
+      for (let j = 0; j < userMap[i].length; j++) {
+        answer[userMap[i][j]]++;
       }
     }
   }
