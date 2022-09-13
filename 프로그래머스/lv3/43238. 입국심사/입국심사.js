cc4ebab4edfbcbfced44c700179ef00792aa1b6d
raon9401 <raon9401@naver.com>
2022-09-13 22:01:21 +0900

    [level 3] Title: 입국심사, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -5,10 +5,9 @@ function solution(n, times) {
   while (min <= max) {
     let mid = Math.floor((min + max) / 2);
 
-    // 중간 값으로 몇명을 심사할 수 있는지 확인.
     let cnt = times.reduce((acc, cur) => (acc += Math.floor(mid / cur)), 0);
+
     if (cnt >= n) {
-      min = Math.min(min, mid);
       max = mid - 1;
     } else {
       min = mid + 1;
