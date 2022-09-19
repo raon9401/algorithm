6e6b33a1c0c26dde906334b52a6c4fbddcb70b0d
raon9401 <raon9401@naver.com>
2022-09-19 19:17:50 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 17528 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.js
+++ 백준/Silver/10773. 제로/제로.js
@@ -1,23 +1,22 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-const caseCount = Number(input[0]);
-const stack =  [];
-
-for (let i = 1; i <= caseCount; i += 1) {
-  const value = Number(input[i]);
-
-  if (value === 0) {
-    stack.pop();
-  } else {
-    stack.push(value);
+function sum() {
+  let stack = [];
+  let result = 0;
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] !== "0") {
+      stack.push(Number(input[i]));
+    } else {
+      stack.pop();
+    }
   }
+  for (let i of stack) {
+    result += i;
+  }
+  return result;
 }
-
-let result = 0;
-
-for (let i = 0; i < stack.length; i += 1) {
-  result += stack[i];
-}
-
-console.log(result);
+console.log(sum());
