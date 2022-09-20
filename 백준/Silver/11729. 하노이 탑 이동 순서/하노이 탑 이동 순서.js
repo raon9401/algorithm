f680af7c44f85871faa7531c356bf2ec3e9c536d
raon9401 <raon9401@naver.com>
2022-09-20 23:46:12 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 940 ms, Memory: 208376 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -0,0 +1,27 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split();
+let N = Number(input[0]);
+
+var answer = [];
+let count = 0;
+let result = "";
+// from : 시작 탑
+// to : 도착 탑
+// other : 경유 탑
+const hanoi = (num, from, to, other) => {
+  if (num === 0) return;
+  hanoi(num - 1, from, other, to);
+  answer.push([from, to]);
+  hanoi(num - 1, other, to, from);
+  count++;
+  return count;
+};
+hanoi(N, 1, 3, 2);
+console.log(count);
+for (let i of answer) {
+  result += `${i[0]} ${i[1]}\n`;
+}
+console.log(result);
