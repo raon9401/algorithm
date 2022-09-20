ec3e0f40c53a318db627fff0d9e1ff5c2c86be6c
raon9401 <raon9401@naver.com>
2022-09-20 23:20:35 +0900

    [level 2] Title: 하노이의 탑, Time: 6.60 ms, Memory: 49.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  var answer = [];
+  const hanoi = (num, from, to, other) => {
+    if (num === 0) return;
+    hanoi(num - 1, from, other, to);
+    answer.push([from, to]);
+    hanoi(num - 1, other, to, from);
+
+    return answer;
+  };
+  return hanoi(n, 1, 3, 2);
+}
