ff49513a23a45501455b090539648e0a7d488ddb
raon9401 <raon9401@naver.com>
2022-09-21 23:40:31 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 9804 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -4,23 +4,9 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-let arr = [];
+let [N, ...arr] = input;
 for (let i = 0; i < input.length - 1; i++) {
   arr[i] = Number(input[i + 1]);
 }
-
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let cur = arr[i];
-    let left = i - 1;
-    while (left >= 0 && arr[left] > cur) {
-      arr[left + 1] = arr[left];
-      arr[left] = cur;
-      left--;
-    }
-  }
+arr.sort((a, b) => a - b);
 console.log(arr.reduce((acc, el) => acc + `${el}\n`, ""));
-
-}
-
-insertionSort(arr);
