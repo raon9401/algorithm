08a12736620e49e5e7ea70c8c7ea860b437ea642
raon9401 <raon9401@naver.com>
2022-09-28 20:44:36 +0900

    [Silver III] Title: N과 M (4), Time: 196 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,34 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+
+let [N, M] = input;
+N = Number(N); // 최대 수
+M = Number(M); // 반복 횟수
+
+let result = "";
+const visited = Array(N).fill(0); // 방문 한지 확인하는 배열
+const output = [];
+
+function dfs(cnt, start) {
+  if (cnt === M) {
+    result += `${output.join(" ")}\n`;
+    return;
+  }
+
+  for (let i = start; i < N; i++) {
+    visited[i] = 1;
+
+    output.push(i + 1);
+    dfs(cnt + 1, i);
+
+    output.pop(); // 1 -> 2 -> 3 -> 4
+
+    visited[i] = 0;
+  }
+}
+
+dfs(0, 0);
+console.log(result);
