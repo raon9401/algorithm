6bd0f6f4c7ca839fb131fea1891b1d951db4c7a6
raon9401 <raon9401@naver.com>
2022-10-04 22:23:18 +0900

    [Silver V] Title: 알고리즘 수업 - 힙 정렬 1, Time: 600 ms, Memory: 90876 KB -BaekjoonHub

--- 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.js
+++ 백준/Silver/24173. 알고리즘 수업 － 힙 정렬 1/알고리즘 수업 － 힙 정렬 1.js
@@ -3,7 +3,6 @@ let input = require("fs")
   .toString()
   .trim()
   .split("\n");
-
 let cnt = 0;
 let result = [];
 let [N, M] = input[0].split(" ").map((el) => Number(el));
@@ -11,6 +10,7 @@ let arr = ["void", ...input[1].split(" ").map((el) => Number(el))];
 
 function heapSort(arr) {
   build_min_heap(arr, N);
+
   for (let i = N; i >= 2; i--) {
     let temp = arr[i];
     arr[i] = arr[1];
@@ -20,17 +20,23 @@ function heapSort(arr) {
     if (cnt === M) {
       result[0] = arr[1];
       result[1] = arr[i];
+      result.sort((a, b) => a - b);
+      console.log(result.join(" "));
+      return;
     }
 
     heapify(arr, 1, i - 1);
   }
 }
 
+// 배열을 힙 자료구조로 만들어주는 과정.
 function build_min_heap(arr, n) {
+  // 자식이 있는 마지막 노드부터 비교
   for (let i = Math.floor(n / 2); i >= 1; i--) {
     heapify(arr, i, n);
   }
 }
+
 function heapify(arr, k, n) {
   let smaller;
   let left = 2 * k;
@@ -53,15 +59,16 @@ function heapify(arr, k, n) {
     if (cnt === M) {
       result[0] = arr[k];
       result[1] = arr[smaller];
+      result.sort((a, b) => a - b);
+      console.log(result.join(" "));
+      return;
     }
+
     heapify(arr, smaller, n);
   }
 }
 heapSort(arr);
-result.sort((a, b) => a - b);
+
 if (cnt < M) {
   console.log(-1);
-} else {
-  console.log(result.join(" "));
 }
-
