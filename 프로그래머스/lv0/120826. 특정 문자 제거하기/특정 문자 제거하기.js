2be16481676fb5739974ae813c43006c81f959a7
raon9401 <raon9401@naver.com>
2023-01-24 15:00:27 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, letter) {
+    var answer = '';
+    for(let i of my_string){
+        if(i !== letter){
+            answer+= i;
+        }
+    }
+    return answer;
+}
