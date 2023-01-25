f064feecb60268cc4e1412ac88a425d7d28f594a
raon9401 <raon9401@naver.com>
2023-01-26 02:54:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,6 @@
 function solution(my_string) {
     var answer = 0;
-    let reg = my_string.replace(/([a-zA-Z])/g,'')
+    let reg = my_string.replace(/[^0-9]/g,'')
     answer = reg.split('').map(i => Number(i)).reduce((a,c) => a+c)
     return answer;
 }
