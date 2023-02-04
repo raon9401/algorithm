ae3049a7199da57a6e97d7b6c51da84655ca4eca
raon9401 <raon9401@naver.com>
2023-02-04 21:03:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,10 +1,13 @@
 function solution(numbers) {
     var answer = 0;
-    const arr = numbers.sort((a,b) => a-b);
+    numbers.sort((a,b) => a-b);
+    answer = Math.max(numbers[0] * numbers[1], numbers[numbers.length-1] * numbers[numbers.length-2])
+    /*
     if(arr[0] * arr[1] >= arr[arr.length-1]*arr[arr.length-2]){
         answer = arr[0] * arr[1];
     } else{
         answer = arr[arr.length-1]*arr[arr.length-2];
     }
+    */
     return answer;
 }
